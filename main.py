0442b4e4d9bf200405dcbe2a0d7a06ef6eb420ec
ARM <alexander.michaud@aya.yale.edu>
2021-02-01 01:04:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# import sys
+# import scipy
+# import numpy
+import matplotlib
+# import sklearn
+import pandas
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length','sepal-width','petal-length','petal-width','class']
+dataset = pandas.read_csv(url, names=names)
+
+# print(dataset.shape)
+# print(dataset.head(20))
+# print(dataset.describe())
+# print(dataset.groupby('class').size())
+
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharey=False)
+matplotlib.pyplot.show()
