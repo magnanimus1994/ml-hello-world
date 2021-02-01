c42a73c7482fb831e8776854facd83bd94501658
ARM <alexander.michaud@aya.yale.edu>
2021-02-01 01:13:51 +0000

    Split training data

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length','sepal-width','petal-length','petal-width','class']
 dataset = pandas.read_csv(url, names=names)
 
+# Dataset Info
 # print(dataset.shape)
 # print(dataset.head(20))
 # print(dataset.describe())
 # print(dataset.groupby('class').size())
 
+# Plotting Data
 # dataset.plot(kind='box', subplots=True, layout=(2,2), sharey=False)
-dataset.hist()
-matplotlib.pyplot.show()
+# dataset.hist()
+# pandas.plotting.scatter_matrix(dataset)
+# matplotlib.pyplot.show()
+
+array = dataset.values
+x = array[:, 0:4]
+y = array[:, 4]
+X_train, X_validation, Y_train, Y_validation = sklearn.model_selection.train_test_split(x, y, test_size=0.20, random_state=1)
