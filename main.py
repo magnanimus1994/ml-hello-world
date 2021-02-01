c9597df6c5bce6a87cbaa92a32de3ab02013a220
ARM <alexander.michaud@aya.yale.edu>
2021-01-31 21:24:26 -0500

    Changed imports

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # import scipy
 # import numpy
 import matplotlib
-# import sklearn
+import sklearn.model_selection
+import sklearn.metrics
 import pandas
 
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
@@ -24,4 +25,4 @@ dataset = pandas.read_csv(url, names=names)
 array = dataset.values
 x = array[:, 0:4]
 y = array[:, 4]
-X_train, X_validation, Y_train, Y_validation = sklearn.model_selection.train_test_split(x, y, test_size=0.20, random_state=1)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(x, y, test_size=0.20, random_state=1)
