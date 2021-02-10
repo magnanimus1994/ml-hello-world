2a31e2cf1dd3a6b3c6923f11900d1284e3bb08f5
ARM <alexander.michaud@aya.yale.edu>
2021-02-10 12:40:23 -0500

    Updated library imports

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-# import sys
-# import scipy
-# import numpy
-import matplotlib
-import sklearn.model_selection
-import sklearn.metrics
-import pandas
+import sys
+import scipy
+import numpy
+
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length','sepal-width','petal-length','petal-width','class']
-dataset = pandas.read_csv(url, names=names)
+dataset = read_csv(url, names=names)
 
 # Dataset Info
 # print(dataset.shape)
@@ -25,4 +37,4 @@ dataset = pandas.read_csv(url, names=names)
 array = dataset.values
 x = array[:, 0:4]
 y = array[:, 4]
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(x, y, test_size=0.20, random_state=1)
+X_train, X_validation, Y_train, Y_validation = train_test_split(x, y, test_size=0.20, random_state=1)
